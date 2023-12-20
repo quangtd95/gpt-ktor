c0df8d6d442f08168ac714a9b45dea1d891a6aa4
quangthaiduy <quang@workforceoptimizer.com>
2023-12-20 15:23:43 +0700

    rename auth api

--- app/src/main/kotlin/com/qtd/modules/auth/api/AuthResource.kt
+++ app/src/main/kotlin/com/qtd/modules/auth/api/AuthResource.kt
@@ -13,36 +13,38 @@ import org.koin.ktor.ext.inject
 fun Route.auth() {
     val authService: IAuthService by inject()
 
-    post("/users") {
-        val registerUser = call.receive<RegisterUserRequest>()
-        val newUser = authService.register(registerUser)
-        call.respond(newUser)
-    }
+    route("/auth") {
+        post("/register") {
+            val registerUser = call.receive<RegisterUserRequest>()
+            val newUser = authService.register(registerUser)
+            call.respond(newUser)
+        }
 
-    post("/users/login") {
-        val loginUser = call.receive<LoginUserRequest>()
-        val user = authService.login(loginUser.user.email, loginUser.user.password)
-        call.respond(user)
-    }
+        post("/login") {
+            val loginUser = call.receive<LoginUserRequest>()
+            val user = authService.login(loginUser.user.email, loginUser.user.password)
+            call.respond(user)
+        }
 
-    post("/users/refresh") {
-        val refreshTokenRequest = call.receive<RefreshTokenRequest>()
-        val userCredentials = authService.refresh(refreshTokenRequest.refreshToken)
-        call.respond(userCredentials)
-    }
-
-    get("/users") {
-        val users = authService.getAllUsers()
-        call.respond(users.map { UserResponse.fromUser(it) })
+        post("/refresh") {
+            val refreshTokenRequest = call.receive<RefreshTokenRequest>()
+            val userCredentials = authService.refresh(refreshTokenRequest.refreshToken)
+            call.respond(userCredentials)
+        }
     }
 
     authenticate("jwt") {
-        get("/user") {
+        get("/users") {
+            val users = authService.getAllUsers()
+            call.respond(users.map { UserResponse.fromUser(it) })
+        }
+
+        get("/users/me") {
             val user = authService.getUserById(call.userId())
             call.respond(UserResponse.fromUser(user))
         }
 
-        put("/user") {
+        put("/user/me") {
             val updateUser = call.receive<UpdateUserRequest>()
             val user = authService.updateUser(call.userId(), updateUser)
             call.respond(UserResponse.fromUser(user))
