df3f3e2af648462b2a787a084b1b9574e2338c0c
quangthaiduy <quang@workforceoptimizer.com>
2023-12-20 10:37:09 +0700

    restructure config

--- infrastructure/ktor/src/main/kotlin/com/qtd/config/Config.kt
+++ infrastructure/ktor/src/main/kotlin/com/qtd/config/Config.kt
@@ -3,21 +3,25 @@ package com.qtd.config
 import io.ktor.server.config.*
 
 fun extractConfig(hoconConfig: HoconApplicationConfig) = config {
-    val config = hoconConfig.config("app")
     server {
-        port = config.property("ktor.deployment.port").getString().toInt()
+        port = hoconConfig.config("ktor.deployment").property("port").getString().toInt()
     }
     jwt {
-        secret = config.property("jwt.secret").getString()
-        realm = config.property("jwt.realm").getString()
-        issuer = config.property("jwt.issuer").getString()
-        audience = config.property("jwt.audience").getString()
+        with(hoconConfig.config("jwt")) {
+            secret = property("secret").getString()
+            realm = property("realm").getString()
+            issuer = property("issuer").getString()
+            audience = property("audience").getString()
+        }
     }
     database {
-        driverClassName = config.property("database.driverClassName").getString()
-        jdbcUrl = config.property("database.jdbcUrl").getString()
-        maximumPoolSize = config.property("database.maximumPoolSize").getString().toInt()
-        isAutoCommit = config.property("database.isAutoCommit").getString().toBoolean()
-        transactionIsolation = config.property("database.transactionIsolation").getString()
+        with(hoconConfig.config("database")) {
+            driverClassName = property("driverClassName").getString()
+            jdbcUrl = property("jdbcUrl").getString()
+            maximumPoolSize = property("maximumPoolSize").getString().toInt()
+            isAutoCommit = property("isAutoCommit").getString().toBoolean()
+            transactionIsolation = property("transactionIsolation").getString()
+        }
+
     }
 }
