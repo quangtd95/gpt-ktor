cb0a13cc5dbdd3d14f4ff50fe3c29158ef040303
quangthaiduy <quang@workforceoptimizer.com>
2023-12-20 10:40:48 +0700

    rename database factory -> database provider

--- infrastructure/ktor/src/main/kotlin/com/qtd/config/Koin.kt
+++ infrastructure/ktor/src/main/kotlin/com/qtd/config/Koin.kt
@@ -2,8 +2,8 @@ package com.qtd.config
 
 import com.auth0.jwt.interfaces.JWTVerifier
 import com.qtd.modules.auth.services.*
-import com.qtd.modules.database.DatabaseFactory
-import com.qtd.modules.database.IDatabaseFactory
+import com.qtd.modules.database.DatabaseProvider
+import com.qtd.modules.database.IDatabaseProvider
 import com.qtd.modules.profile.services.IProfileService
 import com.qtd.modules.profile.services.ProfileService
 import org.koin.dsl.module
@@ -20,5 +20,5 @@ val serviceKoinModule = module {
 }
 
 val databaseKoinModule = module {
-    single<IDatabaseFactory> { DatabaseFactory() }
+    single<IDatabaseProvider> { DatabaseProvider() }
 }
